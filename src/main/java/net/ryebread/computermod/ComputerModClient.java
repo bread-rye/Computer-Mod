2f29dfdb784472762c273e07a850fe535b439316
bread-rye <reuben.anoran@gmail.com>
2023-06-08 19:23:05 +0800

    Test Commit

--- src/main/java/net/ryebread/computermod/ComputerModClient.java
+++ src/main/java/net/ryebread/computermod/ComputerModClient.java
@@ -2,9 +2,10 @@ package net.ryebread.computermod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+//Test
 public class ComputerModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        
+
     }
 }
