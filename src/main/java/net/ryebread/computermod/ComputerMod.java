500f0366d769879934c272af64fd914ad4c784a0
bread-rye <reuben.anoran@gmail.com>
2023-06-08 19:21:01 +0800

    Initial Commit

---
+++ src/main/java/net/ryebread/computermod/ComputerMod.java
@@ -0,0 +1,15 @@
+package net.ryebread.computermod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComputerMod implements ModInitializer {
+    public static final String MOD_ID = "computermod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/ryebread/computermod/ComputerModClient.java
@@ -0,0 +1,10 @@
+package net.ryebread.computermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ComputerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
