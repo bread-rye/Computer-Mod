78cd9544e63df2762a17ee3f6a37ba6521930faf
bread-rye <reuben.anoran@gmail.com>
2023-06-08 21:21:42 +0800

    basic item creation

--- src/main/java/net/ryebread/computermod/item/ModItems.java
+++ src/main/java/net/ryebread/computermod/item/ModItems.java
@@ -3,7 +3,6 @@ package net.ryebread.computermod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,11 +12,11 @@ public class ModItems {
     public static final Item COMPUTER = registerItem(
             "computer",
             new Item(new Item.Settings()),
-            ItemGroups.TOOLS);
+            ModItemGroup.GADGETS);
     public static final Item PHONE = registerItem(
             "phone",
             new Item(new Item.Settings()),
-            ItemGroups.TOOLS);
+            ModItemGroup.GADGETS);
 
     private static Item registerItem(String name, Item item, ItemGroup group) {
         ItemGroupEvents.modifyEntriesEvent(group).register(itemGroup -> itemGroup.add(item));
